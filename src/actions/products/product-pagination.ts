6a8fe8faa15c5691b1fab60eab28e60056d63738
AlbeTorres <albertocorreoficial@gmail.com>
2024-07-01 21:21:49 -0400

    feat: current page and total page to product-pagination

--- src/actions/products/product-pagination.ts
+++ src/actions/products/product-pagination.ts
@@ -1,8 +1,18 @@
 import prisma from '@/lib/prisma'
 
-export const getPaginateProductWithImage = async () => {
-  try {
-    const products = await prisma.product.findMany({
+interface Props {
+  take?: number
+  page?: number
+}
+
+export const getPaginateProductWithImage = async ({ take = 12, page = 1 }: Props) => {
+  if (isNaN(Number(page))) page = 1
+  if (page < 1) page = 1
+
+  const fetchProducts = () =>
+    prisma.product.findMany({
+      take: take,
+      skip: (page - 1) * take,
       include: {
         ProductImages: {
           take: 2,
@@ -13,7 +23,16 @@ export const getPaginateProductWithImage = async () => {
       },
     })
 
+  const fetchTotalCount = () => prisma.product.count({})
+
+  try {
+    const [products, totalCount] = await Promise.all([fetchProducts(), fetchTotalCount()])
+
+    const totalPage = Math.ceil(totalCount / take)
+
     return {
+      currentPage: page,
+      totalPage: totalPage,
       products: products.map(p => ({
         ...p,
         images: p.ProductImages.map(i => i.url),
--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,8 +1,20 @@
 import { getPaginateProductWithImage } from '@/actions'
 import { ProductGrid, Title } from '@/components'
+import { redirect } from 'next/navigation'
 
-export default async function Home() {
-  const { products } = await getPaginateProductWithImage()
+interface Props {
+  searchParams: {
+    page?: string
+  }
+}
+
+export default async function Home({ searchParams }: Props) {
+  const page = searchParams.page ? parseInt(searchParams.page) : 1
+  const { products, currentPage, totalPage } = await getPaginateProductWithImage({ page })
+
+  if (products.length === 0) {
+    redirect('/')
+  }
 
   return (
     <>
