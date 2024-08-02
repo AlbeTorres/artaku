8b3db2e75b6be736219dabaa147afbddd339851d
AlbeTorres <albertocorreoficial@gmail.com>
2024-08-02 19:58:53 -0400

    fix: fix deploy

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -5,6 +5,14 @@ interface Props {
   products: Product[]
 }
 export const ProductGrid = ({ products }: Props) => {
+  if (products.length === 0) {
+    return (
+      <>
+        <h1>No hay productos ...comming soon</h1>
+      </>
+    )
+  }
+
   return (
     <div className='grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4 gap-10 mb-10'>
       {products.map(p => (
