94fffa9eb42ecf2c36548f2bc01cd7094eea58e5
AlbeTorres <albertocorreoficial@gmail.com>
2024-08-02 19:57:19 -0400

    fix: deploy error

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -2,7 +2,6 @@ export const revalidate = 300
 import { getPaginateProductWithImage } from '@/actions'
 import { Pagination, ProductGrid, Title } from '@/components'
 import { parseProducts } from '@/utils/product.parse'
-import { redirect } from 'next/navigation'
 
 interface Props {
   searchParams: {
@@ -13,10 +12,6 @@ interface Props {
 export default async function Home({ searchParams }: Props) {
   const page = searchParams.page ? parseInt(searchParams.page) : 1
   const { products, totalPages } = await getPaginateProductWithImage({ page })
-
-  if (products.length === 0) {
-    redirect('/')
-  }
 
   return (
     <>
